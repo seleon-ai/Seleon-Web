b367501c8ac56466be05ebf91f4a69ee59fc6fbc
tomiimorinigo <morinigotomas1@gmail.com>
2024-09-16 16:19:24 -0300

    Fix mail

--- app/page.tsx
+++ app/page.tsx
@@ -610,7 +610,7 @@ export default function Home() {
                       Email
                     </h3>
                     <Link
-                      href="mailto:your.email@example.com"
+                      href="mailto:contact@seleon.tech"
                       className="inline-flex items-center justify-center rounded-md bg-blue-600 dark:bg-blue-700 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 dark:hover:bg-blue-600 transition-colors"
                     >
                       Enviar email
